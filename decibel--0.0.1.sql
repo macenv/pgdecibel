2d26562b8eb141d482c7468dcac08539a05e35f4
David Bitner <bitner@dbspatial.com>
2015-05-20 15:33:47 -0500

    fix */

--- decibel--0.0.1.sql
+++ decibel--0.0.1.sql
@@ -1,24 +1,9 @@
-/* contrib/citext/citext--1.0.sql */
 
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION citext" to load this file. \quit
+\echo Use "CREATE EXTENSION decibel" to load this file. \quit
 
---
---  PostgreSQL code for CITEXT.
---
--- Most I/O functions, and a few others, piggyback on the "text" type
--- functions via the implicit cast to text.
---
-
---
--- Shell type to keep things a bit quieter.
---
 
 CREATE TYPE decibel;
 
---
---  Input and output functions.
---
 CREATE FUNCTION decibelpascal(float8)
 RETURNS float8
 AS 'MODULE_PATHNAME'
@@ -51,21 +36,26 @@ CREATE OR REPLACE FUNCTION decibel(float4) RETURNS decibel AS 'SELECT $1::text::
 CREATE OR REPLACE FUNCTION decibel(int2) RETURNS decibel AS 'SELECT $1::text::decibel' LANGUAGE SQL IMMUTABLE STRICT COST 1;
 CREATE OR REPLACE FUNCTION decibel(int4) RETURNS decibel AS 'SELECT $1::text::decibel' LANGUAGE SQL IMMUTABLE STRICT COST 1;
 CREATE OR REPLACE FUNCTION decibel(int8) RETURNS decibel AS 'SELECT $1::text::decibel' LANGUAGE SQL IMMUTABLE STRICT COST 1;
+CREATE OR REPLACE FUNCTION decibel(numeric) RETURNS decibel AS 'SELECT $1::text::decibel' LANGUAGE SQL IMMUTABLE STRICT COST 1;
 
+
+CREATE CAST (decibel AS float8) WITHOUT FUNCTION AS IMPLICIT;
 CREATE CAST (float8 AS decibel) WITH FUNCTION decibel(float8) AS IMPLICIT;
 CREATE CAST (float4 AS decibel) WITH FUNCTION decibel(float4) AS IMPLICIT;
 CREATE CAST (int2 AS decibel) WITH FUNCTION decibel(int2) AS IMPLICIT;
 CREATE CAST (int4 AS decibel) WITH FUNCTION decibel(int4) AS IMPLICIT;
 CREATE CAST (int8 AS decibel) WITH FUNCTION decibel(int8) AS IMPLICIT;
+CREATE CAST (numeric AS decibel) WITH FUNCTION decibel(numeric) AS IMPLICIT;
 
-CREATE FUNCTION decibel_sum(decibel,decibel) RETURNS decibel AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
-CREATE FUNCTION decibel_div(decibel,decibel) RETURNS decibel AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
-CREATE FUNCTION decibel_mul(decibel,decibel) RETURNS decibel AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
-CREATE FUNCTION decibel_mi(decibel,decibel) RETURNS decibel AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+CREATE FUNCTION decibel_sum(decibel,decibel) RETURNS decibel AS 'float8pl' LANGUAGE INTERNAL IMMUTABLE STRICT;
+CREATE FUNCTION decibel_div(decibel,float8) RETURNS decibel AS 'float8div' LANGUAGE INTERNAL IMMUTABLE STRICT;
+CREATE FUNCTION decibel_mul(decibel,float8) RETURNS decibel AS 'float8mul' LANGUAGE INTERNAL IMMUTABLE STRICT;
+CREATE FUNCTION decibel_mi(decibel,decibel) RETURNS decibel AS 'float8mi' LANGUAGE INTERNAL IMMUTABLE STRICT;
 
 CREATE OPERATOR + ( PROCEDURE = decibel_sum, LEFTARG=decibel, RIGHTARG=decibel );
-CREATE OPERATOR / ( PROCEDURE = decibel_div, LEFTARG=decibel, RIGHTARG=decibel );
-CREATE OPERATOR * ( PROCEDURE = decibel_mul, LEFTARG=decibel, RIGHTARG=decibel );
+CREATE OPERATOR / ( PROCEDURE = decibel_div, LEFTARG=decibel, RIGHTARG=float8 );
+CREATE OPERATOR * ( PROCEDURE = decibel_mul, LEFTARG=decibel, RIGHTARG=float8 );
 CREATE OPERATOR - ( PROCEDURE = decibel_mi, LEFTARG=decibel, RIGHTARG=decibel );
 
-CREATE AGGREGATE sum(decibel) ( sfunc = decibel_sum, stype = decibel );
+CREATE AGGREGATE sum(decibel) ( sfunc = float8pl, stype = float8 );
+CREATE AGGREGATE avg(decibel) ( sfunc = float8_accum, stype = float8[], finalfunc=float8_avg, initcond='{0,0,0}' );
